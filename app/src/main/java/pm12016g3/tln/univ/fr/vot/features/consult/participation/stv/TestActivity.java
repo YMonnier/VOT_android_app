c45bb57c22f71f597574103f2ac65e01b9185c0b
Ysee Monnier <yseemonnier@gmail.com>
2017-05-22 11:05:16 +0200

    test package

---
+++ app/src/main/java/pm12016g3/tln/univ/fr/vot/features/consult/participation/stv/TestActivity.java
@@ -0,0 +1,13 @@
+package pm12016g3.tln.univ.fr.vot.features.consult.participation.stv;
+
+/**
+ * Project android.
+ * Package pm12016g3.tln.univ.fr.vot.features.consult.participation.stv.
+ * File TestActivity.java.
+ * Created by Ysee on 22/05/2017 - 11:04.
+ * www.yseemonnier.com
+ * https://github.com/YMonnier
+ */
+
+public class TestActivity {
+}
