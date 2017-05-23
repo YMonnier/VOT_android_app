8de4b920cba5a2d3b06ec7a7b7f17febbed492e6
Ysee Monnier <yseemonnier@gmail.com>
2017-05-23 15:07:21 +0200

    add Social Choix data model

---
+++ app/src/main/java/pm12016g3/tln/univ/fr/vot/models/shared/SCMajorityJudgment.java
@@ -0,0 +1,12 @@
+package pm12016g3.tln.univ.fr.vot.models.shared;
+
+/**
+ * Project android.
+ * Package pm12016g3.tln.univ.fr.vot.models.shared.
+ * File SCMajorityJudgment.java.
+ * Created by Ysee on 23/05/2017 - 10:21.
+ * www.yseemonnier.com
+ * https://github.com/YMonnier
+ */
+
+public class SCMajorityJudgment implements SCData {}
---
+++ app/src/main/java/pm12016g3/tln/univ/fr/vot/models/shared/SCSimpleTransfarableVote.java
@@ -0,0 +1,35 @@
+package pm12016g3.tln.univ.fr.vot.models.shared;
+
+/**
+ * Project android.
+ * Package pm12016g3.tln.univ.fr.vot.models.shared.
+ * File SCSimpleTransfarableVote.java.
+ * Created by Ysee on 23/05/2017 - 10:21.
+ * www.yseemonnier.com
+ * https://github.com/YMonnier
+ */
+
+public class SCSimpleTransfarableVote implements SCData {
+    enum Elimination {
+        LESS("Less"),
+        OCCURENCE("Occurence");
+        private String value;
+
+        private Elimination(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    private int nbWinner;
+    private Elimination elimination;
+
+    public SCSimpleTransfarableVote(int nbWinner, Elimination elimination) {
+        this.nbWinner = nbWinner;
+        this.elimination = elimination;
+    }
+}
