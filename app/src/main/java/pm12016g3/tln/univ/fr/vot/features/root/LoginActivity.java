37176d09e2badf040c11c5245123a187a9103efe
Damien Lemenager <pokemonedward@hotmail.fr>
2017-06-07 02:25:30 +0200

    Add better view for JM

--- app/src/main/java/pm12016g3/tln/univ/fr/vot/features/root/LoginActivity.java
+++ app/src/main/java/pm12016g3/tln/univ/fr/vot/features/root/LoginActivity.java
@@ -278,8 +278,10 @@ public class LoginActivity extends AppCompatActivity
 
                     Log.i(TAG, "Login User...");
                     Log.i(TAG, GsonSingleton.getInstance().toJson(user));
+                    System.out.println("token device 2: "+token);
+
                     ResponseEntity<Response<User>> response = apiService.login(user);
-                    Log.i(TAG, response.toString());
+                    Log.i(TAG, "resposne "+response.toString());
 
                     if (response.getStatusCode().is2xxSuccessful()) {
                         Settings.currentUser = response.getBody().getData();
